7a77ad9b718b7cbe3d000d943ad1188c92cd240f
Sabdar <sabdarshaikh10@gmai.com>
2024-07-11 00:24:26 +0530

    connecting frontend to backend

--- routes/posts.js
+++ routes/posts.js
@@ -1,90 +1,25 @@
 import express from 'express';
+import {getPosts,getPost, createPost,updatePost, deletePost} from '../controllers/postController.js'
 const router = express.Router();
 
-let posts = [
-    {id: 1 , title: 'Post One'},
-    {id: 2 , title: 'Post Two'},
-    {id: 3 , title: 'Post Three'},
-];
+
 
 // GET all posts
-router.get('/' , (req, res , next) => {
-    const limit = parseInt(req.query.limit);
-
-    if (!isNaN(limit) && limit > 0) {
-       return res.status(200).json(posts.slice(0, limit));
-    }
-   
-    res.status(200).json(posts);
-});
+router.get('/' , getPosts);
 
 
 // GET single post
-router.get('/:id' , (req, res, next) => {
-    const id = parseInt(req.params.id);
-    const post = posts.find((post => post.id === id));
-
-   if(!post) {
-    const error = new Error (`A post with id of ${id} was not found`);
-    error.status = 404;
-    return next(error);
-   }
-    res.status(200).json(post);
-
-});
+router.get('/:id' , getPost);
 
 // creating a new post
 
-router.post('/' , (req,res , next) => {
-    const newPost = {
-        id: posts.length+1,
-        title: req.body.title
-    };
-
-    if (!newPost.title) {
-        const error = new Error (`Please include a title`);
-        error.status = 400;
-        return next(error);
-    }
-    posts.push(newPost);
-    res.status(201).json(posts);
-});
+router.post('/' , createPost);
 
 
 //Update Post 
-router.put('/:id' , (req,res, next) => {
-    const id = parseInt(req.params.id);
-    const post = posts.find((post) => post.id === id);
-    
-    if (!post) {
-        const error = new Error (`A post with id of ${id} was not found`);
-    error.status = 404;
-    return next(error);
-
-
-    }
-
-    post.title = req.body.title;
-    res.status(200).json(posts);
-
-}); 
+router.put('/:id' , updatePost); 
 
 //Delete Post 
-router.put('/:id' , (req,res, next) => {
-    const id = parseInt(req.params.id);
-    const post = posts.find((post) => post.id === id);
-    
-    if (!post) {
-        const error = new Error (`A post with id of ${id} was not found`);
-        error.status = 404;
-        return next(error);
-
-
-    }
-
-    posts = posts.filter((post => post.id !== id));
-    res.status(200).json(posts);
-
-});
+router.delete('/:id' , deletePost);
 
 export default router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import posts from './routes/posts.js';
 import logger from './middleware/logger.js';
 import errorHandler from './middleware/error.js';
@@ -8,35 +9,43 @@ import notFound from './middleware/notFound.js';
 const port = process.env.PORT || 8000;
 const app = express();
 
-// setup static folder
-
- //app.use(express.static(path.join(__dirname, 'public')));
- 
-// app.get('/' , (req, res) => {
-  // res.send ('<h1>Hello World</h1>')
-    //res.send ({message : 'Hello World'})
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'))
-// })
-
-// app.get('/about', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'about.html'))
-// })
+// Get directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //Body Parser middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-
-//Logger middleware
-app.use(logger);
-
-
-
-// Routes
-app.use('/api/posts', posts );
-
+  
+  //Logger middleware
+  app.use(logger);
+  
+ // setup static folder
+  
+   app.use(express.static(path.join(__dirname, 'public')));
+  
+  
+  // Routes
+  app.use('/api/posts', posts );
+  
 //ErrorHandler
 app.use(notFound);
 app.use(errorHandler);
 
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+
+
+
+
+
+// app.get('/' , (req, res) => {
+  // res.send ('<h1>Hello World</h1>')
+  //res.send ({message : 'Hello World'})
+  //     res.sendFile(path.join(__dirname, 'public', 'index.html'))
+  // })
+  
+// app.get('/about', (req, res) => {
+  //     res.sendFile(path.join(__dirname, 'public', 'about.html'))
+  // })
